b763c01dcd4634c3ff6c7ab702847027003b1802
Luke Sweeney <luke@thesweeneys.org>
2017-06-04 11:57:35 -0500

    refactors build a bit

--- lib/build.rb
+++ lib/build.rb
@@ -5,18 +5,10 @@ require_relative "population"
 $target_phrase = "VSCode is cool"
 population = Population.new
 
-population.calc_fitness
-population.natural_selection
-
-# def evolve
-#   # Key Algo right here
-#   population.calc_fitness
-#   population.natural_selection
-#   population.generate
-#   population.evaluate
-
-# end
-
 # until population.is_finished
-#   evolve
+  # Key Algo right here
+  population.calc_fitness
+  population.natural_selection
+  # population.generate
+  # population.evaluate
 # end
